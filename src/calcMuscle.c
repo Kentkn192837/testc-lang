9093b5fbc20b8c474510f9f7bfbf7734f131145a
Kentkn192837 <agriculture5012@gmail.com>
2021-01-07 14:20:07 +0900

    骨格筋率を計算するプログラム

---
+++ src/calcMuscle.c
@@ -0,0 +1,31 @@
+//////////////////////////////////
+//  骨格筋量を計算するプログラム  //
+//////////////////////////////////
+
+#include <stdio.h>
+
+float muscle(float weight, float rateOffat);
+
+int main()
+{
+    float weight;
+    float rateOffat;
+    float result;
+
+    printf("体重を入力してください: ");
+    scanf("%f", &weight);
+    printf("体脂肪率を入力してください: ");
+    scanf("%f", &rateOffat);
+
+    result = muscle(weight, rateOffat);
+    printf("骨格筋率: %f", result);
+}
+
+float muscle(float weight, float rateOffat)
+{
+    float fat = weight * (rateOffat / 100);
+    float rmFat = weight - fat;
+    float muscle = rmFat / 2;
+    float rateOfmuscle = muscle / weight * 100;
+    return rateOfmuscle;
+}
