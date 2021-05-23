b60d574c9f293f034f699f1d5d627357dd5a2242
Kentkn192837 <agriculture5012@gmail.com>
2021-05-24 04:45:24 +0900

    生成した乱数を保存する処理

--- src/kadai5.c
+++ src/kadai5.c
@@ -1,18 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 100
-#define A 0
-#define B 2
+#define N 1000
+#define A -1
+#define B 1
 
 int main(void)
 {
     unsigned int seed = 12345;
-    double x[N];
+    double x[N], y[N], z[N];
     srand(seed);
     for (int i = 0; i < N; i++){
         x[i] = (double)rand() / RAND_MAX * (B - A) + A;
-        printf("%3.2lf\n", x[i]);
+        y[i] = (double)rand() / RAND_MAX * (B - A) + A;
+        z[i] = (double)rand() / RAND_MAX * (B - A) + A;
+        // printf("x[%d] = %3.2lf\n", i, x[i]);
+        // printf("y[%d] = %3.2lf\n", i, y[i]);
+        // printf("z[%d] = %3.2lf\n", i, z[i]);
     }
+
+    FILE *fp;
+    char *filename = "data.txt";
+    fp = fopen(filename, "w");
+    if (fp == NULL) {
+        printf("Can't open this file.");
+        exit(1);
+    }
+
+    for (int i = 0; i < N; i++) {
+        fprintf(fp, "%lf,", x[i]);
+        fprintf(fp, "%lf,", y[i]);
+        fprintf(fp, "%lf\n", z[i]);
+    }
+
     return 0;
 }
