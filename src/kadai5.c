68125e0a21ad305f5da9eb3e12706a4191668561
Kentkn192837 <agriculture5012@gmail.com>
2021-05-28 01:24:24 +0900

    一様乱数で球を生成

--- src/kadai5.c
+++ src/kadai5.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-#define N 1000
+#define N 10000
 #define A -1
 #define B 1
 
 int main(void)
 {
     unsigned int seed = 12345;
-    double x[N], y[N], z[N];
+    double x[N], y[N], z[N], phi[N];
+
     srand(seed);
     for (int i = 0; i < N; i++){
-        x[i] = (double)rand() / RAND_MAX * (B - A) + A;
-        y[i] = (double)rand() / RAND_MAX * (B - A) + A;
+        // -1‚©‚ç1‚Ì”ÍˆÍ‚Åz‚Ìˆê—l—”‚ð¶¬
         z[i] = (double)rand() / RAND_MAX * (B - A) + A;
-        // printf("x[%d] = %3.2lf\n", i, x[i]);
-        // printf("y[%d] = %3.2lf\n", i, y[i]);
-        // printf("z[%d] = %3.2lf\n", i, z[i]);
+        // 0‚©‚ç2ƒÎ‚Ì”ÍˆÍ‚Åˆê—l—”‚ð¶¬
+        phi[i] = (double)rand() / RAND_MAX * (M_PI * 2);
+        
+        x[i] = sqrt( 1 - pow(z[i], 2.0) ) * cos(phi[i]);
+        y[i] = sqrt( 1 - pow(z[i], 2.0) ) * sin(phi[i]);
     }
 
     FILE *fp;
-    char *filename = "data.txt";
+    char *filename = "newdata.txt";
     fp = fopen(filename, "w");
     if (fp == NULL) {
         printf("Can't open this file.");
@@ -30,7 +33,8 @@ int main(void)
     for (int i = 0; i < N; i++) {
         fprintf(fp, "%lf,", x[i]);
         fprintf(fp, "%lf,", y[i]);
-        fprintf(fp, "%lf\n", z[i]);
+        fprintf(fp, "%lf,", z[i]);
+        fprintf(fp, "%lf\n", phi[i]);
     }
 
     fclose(fp);
