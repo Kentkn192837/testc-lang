ad81b8e21d3bc19850b6755f1f139408fd8e37ac
Kentkn192837 <agriculture5012@gmail.com>
2021-09-15 23:52:46 +0900

    std::vector_1

---
+++ src/C++/12.4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main()
+{
+    std::string str = "apple";
+    std::vector<char> cv{ str.begin(), str,end() };
+
+    for (char c : cv)
+    {
+        std::cout << c << std::endl;
+    }
+}
